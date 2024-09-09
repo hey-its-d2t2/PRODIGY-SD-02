221b3b999fce7a43dca4b3a17175a04d849d5ac0
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-09 13:04:46 +0530

    Initial commit, Project created from start.spring.io with thes config, Java 17, Springboot version 3.3.3, pack - jar, dependency spring web,dev tools, lombok.

---
+++ src/test/java/com/PRODIGY_SD_02/ProdigySd02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PRODIGY_SD_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdigySd02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
