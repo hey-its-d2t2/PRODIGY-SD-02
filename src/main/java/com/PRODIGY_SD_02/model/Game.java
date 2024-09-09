6fd51c6a664e516e439c8426c7d66c60828540a2
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-09 13:09:55 +0530

    Model implemented.

--- src/main/java/com/PRODIGY_SD_02/model/Game.java
+++ src/main/java/com/PRODIGY_SD_02/model/Game.java
@@ -1,4 +1,39 @@
 package com.PRODIGY_SD_02.model;
 
 public class Game {
+    private int targetNumber;
+    private int attempts;
+    private String message;
+
+    public Game() {}
+
+    public Game(int targetNumber, int attempts, String message) {
+        this.targetNumber = targetNumber;
+        this.attempts = attempts;
+        this.message = message;
+    }
+
+    public int getTargetNumber() {
+        return targetNumber;
+    }
+
+    public void setTargetNumber(int targetNumber) {
+        this.targetNumber = targetNumber;
+    }
+
+    public int getAttempts() {
+        return attempts;
+    }
+
+    public void setAttempts(int attempts) {
+        this.attempts = attempts;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
